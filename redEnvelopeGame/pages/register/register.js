4abec2cd2a9cbd90ab6a7abc5e9bcd9768d48a87
LiangBo <369062071@qq.com>
2018-01-12 18:24:29 +0800

    1
    1

--- redEnvelopeGame/pages/register/register.js
+++ redEnvelopeGame/pages/register/register.js
@@ -5,9 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    sex: ['先生', '女士'],
+    showPick: 0,
+    uName:'',
+    uSex:''
   },
-
+  pickTap () {
+    let _showPick = this.data.showPick++
+    this.setData({
+      showView: _showPick
+    })
+    console.log(this.data.showPick)
+  },
+  // 选择性别
+  selectItemTap (e) {
+    let currentSex = e.currentTarget.dataset.text
+    console.log(e)
+    let _showPick = this.data.showPick++
+    this.data.submitSex = currentSex
+    this.setData({
+      sexPick: currentSex,
+      showView: _showPick
+    })
+  },
+  // getUserName (e) {
+  //   this.setData({
+  //     uName: e.detail.value.uName
+  //   })
+  // },
+  getIptVal (e) {
+    console.log(e)
+    this.setData({
+      uName: e.detail.value
+    })
+  },
+  getUsex (e) {
+    this.data.uSex = e.currentTarget.dataset.text;
+    console.log(this.data.uSex)
+  },
+  submitTap () {
+    let uName = this.data.uName
+    
+    let uSex = this.data.uSex
+  
+    console.log('贵姓：', uName, uSex)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
